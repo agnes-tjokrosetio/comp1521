b1287c25b761c5b43d08aee677377d5c353d780c
Agnes <z5477074@ad.unsw.edu.au>
2025-11-11 19:05:24 +1100

    tut9

--- tut9/chmod_if_public_write.c
+++ tut9/chmod_if_public_write.c
@@ -16,10 +16,21 @@ void chmod_if_needed(char *pathname);
 int main(int argc, char *argv[]) {
     // TODO
 
+    for (int arg = 1; arg < argc; arg++) {
+        chmod_if_needed(argv[arg]);
+    }
+
     return 0;
 }
 
 // function to chmod a file if publically-writeable
 void chmod_if_needed(char *pathname) {
     // TODO
+    struct stat s;
+    stat(pathname, &s);
+    int mode = s.st_mode & ~S_IWOTH;
+    // 111 111 111
+    // 111 111 101
+    // ~S_IWOTH
+    chmod(pathname, mode);
 }
--- tut9/count_utf.c
+++ tut9/count_utf.c
@@ -19,10 +19,44 @@ int codepoint_length(char byte);
 
 int main(int argc, char **argv) {
     // TODO
+
+    if (argc < 2) {
+        exit(1);
+    }
+
+    char *utf8_string = argv[1];
+    int i = 0;
+    int count_chars = 0;
+
+    while (utf8_string[i] != '\0') {
+        int length = codepoint_length(utf8_string[i]);
+        i += length;
+        count_chars += 1;
+    }
+
+    printf("there are %d codepoints in the string\n", count_chars);
 }
 
 
 // function to get the length of each utf-8 char
 int codepoint_length(char byte) {
     // TODO
+    // 0xxxxxxx -> 1 byte length
+    if ((byte & 0b10000000) == 0b00000000) {
+        return 1;
+    }
+    // 110xxxxx -> 2 byte length
+    else if ((byte & 0b11100000) == 0b11000000) {
+        return 2;
+    }
+    // 1110xxxx -> 3 bytes
+    else if ((byte & 0b11110000) == 0b11100000) {
+        return 3;
+    }
+    // 11110xxx -> 4 bytes
+    else if ((byte & 0b11111000) == 0b11110000) {
+        return 4;
+    }
+
+    return 0;
 }
