ee1f61665cd4d77cb23f01f2680de83339002455
Agnes <135120512+agnes-tjokrosetio@users.noreply.github.com>
2026-03-12 14:47:18 +1100

    week four

--- tut4/largest_element.s
+++ tut4/largest_element.s
@@ -6,17 +6,17 @@
         .text
 
 max:
-	# Frame:    [$ra, $s0]   			=>  All the registers that are pushed and popped
+	# Frame:    [$ra, $s0]   					=>  All the registers that are pushed and popped
 	# Uses:     [$ra, $s0, $a0, $a1, $v0]		=>  Every register you have used
-	# Clobbers: [$a0, $a1, $v0]			=>  Clobbers = Uses - Frame (registers that don't preserve values)
+	# Clobbers: [$a0, $a1, $v0]					=>  Clobbers = Uses - Frame (registers that don't preserve values)
 	#
-	# Locals:           				=>  Variables defined or used in the function
+	# Locals:           						=>  Variables defined or used in the function
 	# - $s0: int first_element
 	# - $v0: int max_so_far
 	# - $a0: int array[], &array[1]
 	# - $a1: int length, length - 1
 	#
-	# Structure:        				=>  Labels you create
+	# Structure:        						=>  Labels you create
 	#   max
 	#   -> max__prologue
 	#   -> max__body
@@ -25,35 +25,35 @@ max:
 	#	-> max__body_so_far
 	#   -> max__epilogue
 max__prologue:
-        begin					# create stack
+        begin								# create stack
         push    $ra
         push    $s0
 
 max__body:
-        lw      $s0, ($a0)			# int first_element = array[0];
+        lw      $s0, ($a0)					# int first_element = array[0];
 
         bne     $a1, 1, max__body_recurse 	# if (length = 1) go to base_case, otherwise do recurse
 
 max__body_base_case:
-        move    $v0, $s0			# return first_element;
+        move    $v0, $s0					# return first_element;
         j       max__epilogue
 
 max__body_recurse:
-        add     $a0, $a0, 4			# int max_so_far = max(&array[1], length - 1);
+        add     $a0, $a0, 4					# int max_so_far = max(&array[1], length - 1);
         sub     $a1, $a1, 1
         jal     max
 
         ble     $s0, $v0, max__epilogue		# if (first_element > max_so_far) max_so_far = first_element;
 
 max__body_so_far:
-        move    $v0, $s0			# return max_so_far;
+        move    $v0, $s0					# return max_so_far;
 
 max__epilogue:
-        pop     $s0				# clean up stack
+        pop     $s0							# clean up stack
         pop     $ra
         end
 
-        jr      $ra				# return from max
+        jr      $ra							# return from max
 
 
 # ==============================================================================
@@ -66,7 +66,7 @@ main__prologue:
 main__body:
 	la	$a0, array
 	li	$a1, 10
-	jal	max			# result = max(array, 10)
+	jal	max				# result = max(array, 10)
 
 	move	$a0, $v0
 	li	$v0, 1			# syscall 1: print_int
@@ -77,7 +77,7 @@ main__body:
 	syscall				# printf("%c", '\n');
 
 main__epilogue:
-	pop	$ra			# clean up stack
+	pop	$ra				# clean up stack
 	end
 	
 	li	$v0, 0			# return 0;
@@ -91,4 +91,5 @@ main__epilogue:
 
 array:
 	.word 1, 2, 3, 4, 5, 6, 4, 3, 2, 1
+
 
